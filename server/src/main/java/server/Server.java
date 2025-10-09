5c8b753121536efd333425d9f5b73244b1bc1e04
drewrigby <drewrigb@byu.edu>
2025-10-09 10:30:42 -0600

    Phase 3

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,42 @@
+package server;
+
+import com.google.gson.Gson;
+import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
+
+public class Server {
+
+    private final Javalin server;
+
+    public Server() {
+        server = Javalin.create(config -> config.staticFiles.add("web"));
+
+        server.delete("db",ctx -> ctx.result("{}"));
+        //endpoints
+        server.post("user", ctx->register(ctx));
+
+    }
+
+    private void register (Context ctx) {
+        var serializer = new Gson();
+        String reqJson = ctx.body();
+        var req = serializer.fromJson(reqJson, Map.class);
+
+        //call to the service and register
+
+        var res = Map.of("username", req.get("username"),"authToken","yzx");
+        ctx.result(serializer.toJson(res));
+
+    }
+
+    public int run(int desiredPort) {
+        server.start(desiredPort);
+        return server.port();
+    }
+
+    public void stop() {
+        server.stop();
+    }
+}
